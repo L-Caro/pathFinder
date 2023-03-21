c45e311aae91bbf8a71f8df22a16ba9850f997ff
L-Caro <lionelcaro74@gmail.com>
2023-03-21 23:53:44 +0100

    fichier SQL avec toutes les données terminé !

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,27 +1,39 @@
 const { Trek } = require("../models/index");
+const path = require('path');
+const fs = require("fs");
 
 const mainController = {
     async homePage(req, res){
+        const trek = await Trek.findByPk(1, {
+            include: [
+                {
+                    association: 'difficulty',
+                    attributes: ['name'],
+                },
+                {
+                    association: 'summit',
+                    attributes: ['name'],
+                },
+                {
+                    association: 'tags',
+                    attributes: ['name'],
+                    through: { attributes: [] },
+                }
+            ]
+        });
+        // === Gestion de la demande de l'affichage des photos ===
+        // Il faut require fs et path en haut de la page
+        const id = 4 // A remplacer par req.params.id le moment venu
         try {
-            const trek = await Trek.findByPk(1, {
-                include: [
-                    {
-                        association: 'difficulty',
-                        attributes: ['name'],
-                    },
-                    {
-                        association: 'summit',
-                        attributes: ['name'],
-                    },
-                    {
-                        association: 'tags',
-                        attributes: ['name'],
-                        through: { attributes: [] },
-                    }
-                ]
-            });
-                console.log(JSON.stringify(trek, null, 2));
-            res.render('home', { trek });
+            const photosfields = path.join(__dirname, `../../public/images/assets/${id}/XS`); // id en dynamique
+            fs.readdir(photosfields, (err, files) => {
+                if (err) {
+                    console.trace(err);
+                    return res.status(500).send('Erreur lors de la récupération des photos')
+                }
+        // === Fin de la gestion de l'affichage des photos ===
+            res.render('home', { trek, photos: files }); // Envoie à la page EJS de la variable files| Trek c'est les infos écrites
+        });
         } catch (error) {
             console.trace(error);
             res.status(500).render("error/500");

094c5efadae80bf50f929598b08891cdb3805187
L-Caro <lionelcaro74@gmail.com>
2023-03-25 10:49:20 +0100

    gestion du cadre externe par js et qr codes enregistrés

--- public/js/script.js
+++ public/js/script.js
@@ -6,9 +6,24 @@ function adjustMainRightHeight() {
     document.querySelector('.trek_main-right').style.height = leftHeight + 'px';
   }
 }
-  
+
+// Au 1er chargement de la page 
 if (window.innerWidth > 1200) {
   window.addEventListener('load', adjustMainRightHeight);
   window.addEventListener('resize', adjustMainRightHeight);
 }
+
+// Des que la fenêtre est redimensionnée
+window.addEventListener('resize', function() {
+  if (window.innerWidth > 1200) {
+    window.addEventListener('load', adjustMainRightHeight);
+    window.addEventListener('resize', adjustMainRightHeight);
+  } else {
+    window.removeEventListener('load', adjustMainRightHeight);
+    window.removeEventListener('resize', adjustMainRightHeight);
+  }
+});
+
+
+
 

d72f30b2146891087d396b9c28274fef5393a10e
L-Caro <lionelcaro74@gmail.com>
2023-03-27 22:23:29 +0200

    header burger terminé, Sprint 1 terminé

--- public/js/script.js
+++ public/js/script.js
@@ -24,3 +24,21 @@ window.addEventListener('resize', function() {
   }
 });
 
+
+
+// Menu burger du header
+// == Menu burger == //
+let sidenav = document.getElementById("mySidenav");
+let openBtn = document.getElementById("openBtn");
+let closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+function openNav() {
+  sidenav.classList.add("active");
+}
+function closeNav() {
+  sidenav.classList.remove("active");
+}
+// == == //
